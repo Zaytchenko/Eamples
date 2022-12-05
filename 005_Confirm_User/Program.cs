d03533b21a9eab067d65ca488f187cede2b63b29
Zaytchenko_Yuriy <Zaytchenko@gmail.com>
2022-12-05 21:24:13 +0200

    add 005 Confirm_User

---
+++ 005_Confirm_User/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пльзователя ");
+string username = Console.ReadLine ();
+
+if (username.ToLower() == "маша")
+{Console.WriteLine ("Ура! Это же Маша!");
+}
+else
+{
+    Console.Write("Привет,");
+    Console.WriteLine (username);
+}
