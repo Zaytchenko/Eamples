6294b7828b285233c5519606b16acb70a44f58f6
Zaytchenko_Yuriy <Zaytchenko@gmail.com>
2022-12-08 20:44:58 +0200

    add 011_ArrayLibrary

---
+++ 011_ArrayLibrary/Program.cs
@@ -0,0 +1,47 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int i = 0;
+    while (i < length)
+    {
+        collection[i] = new Random().Next(1, 10);
+        i++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int j = 0;
+    while (j < count)
+    {
+        Console.WriteLine(col[j]);
+        j++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int i = 0;
+    int position = -1;
+    while (i < count)
+    {
+        if (collection[i] == find)
+        {
+            position = i;
+            break;
+        }
+         i++;
+    }
+  return position; 
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos =IndexOf(array, 4);
+Console.WriteLine(pos);
