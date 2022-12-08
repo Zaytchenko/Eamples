b7d9c965c309ee4868f86c5a693429ff54b944cf
Zaytchenko_Yuriy <Zaytchenko@gmail.com>
2022-12-08 14:11:01 +0200

    add 010_MethodArray

---
+++ 010_MethodArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = { 1, 12, 31, 14, 18, 51, 16, 71, 18 };
+int n = array.Length;
+int find = 18;
+int i = 0;
+while (i < n)
+{
+    if (array[i] == find)
+    {
+        Console.WriteLine(i);
+        break;
+    }
+    i++;
+}
